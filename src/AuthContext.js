2edd0a91e82addc5f24845604e8ba46429035035
rajpraneeth <rajpraneeth54@gmail.com>
2024-01-18 14:54:08 +0530

    adding session storage

--- src/AuthContext.js
+++ src/AuthContext.js
@@ -1,22 +1,27 @@
 // AuthContext.js
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext } from "react";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState({
-    authenticated: false,
-    role: 'user', // Default role is 'user'
+  const [user, setUser] = useState(() => {
+    const storedUser = sessionStorage.getItem("user");
+    return storedUser
+      ? JSON.parse(storedUser)
+      : { authenticated: false, role: "user" };
   });
 
   const login = () => {
     // Your login logic here
-    setUser({ authenticated: true, role: 'user' });
+    const newUser = { authenticated: true, role: "user" };
+    setUser(newUser);
+    sessionStorage.setItem("user", JSON.stringify(newUser));
   };
 
   const logout = () => {
     // Your logout logic here
-    setUser({ authenticated: false, role: 'user' });
+    setUser({ authenticated: false, role: "user" });
+    sessionStorage.removeItem("user");
   };
 
   return (
@@ -29,7 +34,7 @@ export const AuthProvider = ({ children }) => {
 export const useAuth = () => {
   const context = useContext(AuthContext);
   if (!context) {
-    throw new Error('useAuth must be used within an AuthProvider');
+    throw new Error("useAuth must be used within an AuthProvider");
   }
   return context;
 };
